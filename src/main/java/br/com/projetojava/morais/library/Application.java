6e56be1ac0d6c7016e13aa10e168b81f70e6c5a6
ProjetoAlanaJava <73372877+ProjetoAlanaJava@users.noreply.github.com>
2020-10-30 18:12:40 -0300

    Update Application.java
    Alterando a app.java

--- src/main/java/br/com/projetojava/morais/library/Application.java
+++ src/main/java/br/com/projetojava/morais/library/Application.java
@@ -2,6 +2,13 @@ package br.com.projetojava.morais.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +16,18 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	private static Connection getConnection() throws SQLException, URISyntaxException {
+
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "dbc:mysql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
 
 }
