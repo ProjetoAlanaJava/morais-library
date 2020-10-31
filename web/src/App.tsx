890796952a76772b0c6d56575a72ef405313ea79
Igor Sales <igorsales.fs@gmail.com>
2020-10-30 22:07:36 -0300

    add Navbar component and add in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 import './assets/styles/global.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes />
     </BrowserRouter>
   );
---
+++ web/src/components/Navbar/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+
+import logoutIcon from '../../assets/images/icons/logout.svg';
+import logoExtendIcon from '../../assets/images/icons/logo-extend.svg';
+
+import './styles.css';
+
+function Navbar(){
+
+    const handleLogout = () => {
+      console.log('LOGOUT');
+    }
+
+    return (
+        <header className="page-header">
+            <Link to="/">
+                <img src={logoExtendIcon} alt="Logo"/>
+            </Link>
+            <nav className="navbar">
+                <ul className="menu">
+                    <li className="menu-item">
+                        <Link to="/document">Livros</Link>
+                    </li>
+                    <li className="menu-item">
+                        <Link to="/company">Usuários</Link>
+                    </li>
+                    <li className="menu-item">
+                        <Link to="/settings">Configurações</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="logout">
+                <span>Olá, User</span>
+                <Link to="#">
+                    <img src={logoutIcon} alt="logout" onClick={handleLogout}/>
+                </Link>
+            </div>
+        </header>
+      
+    )
+}
+
+export default Navbar;
