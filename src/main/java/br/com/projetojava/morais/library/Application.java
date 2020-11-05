a02ee3f9a097ff9bfc98ce71c308d8f137dbce51
Lacenlot <gabrieloliveira.17@outlook.com>
2020-11-05 17:03:21 -0300

    adicionandoCors*

--- src/main/java/br/com/projetojava/morais/library/Application.java
+++ src/main/java/br/com/projetojava/morais/library/Application.java
@@ -19,7 +19,7 @@ public class Application {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("https://moraislibrary-frontend.herokuapp.com/")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
