59579063f5bd726b4af0e5559dd75218aac87d6b
Igor Sales <igorsales.fs@gmail.com>
2020-11-05 23:52:33 -0300

    adicionando dados dinamicos e dispatch de action usina action usando redux

--- web/src/Pages/UsersList/index.tsx
+++ web/src/Pages/UsersList/index.tsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListItem from '../../components/ListItem';
 import PageBody from '../../components/PageBody';
+
+import { ApplicationState } from '../../store';
+import { loadUsersRequest } from '../../store/modules/users/actions';
+
+import { User } from '../../utils/@types/Users';
 
 import avatar from '../../assets/images/avatar.png';
 
 import './styles.css'
 
+
 function UsersList(){
-  
+
+  const dispatch = useDispatch()
+
+  const { users } = useSelector( (state: ApplicationState) => state);
+
+  useEffect(() => {
+    dispatch(loadUsersRequest())
+}, [dispatch]);
+
+
   return (
     <PageBody 
       title="Usuários - Lista"
       link="/usuarios/form"
       isForm={false}
     >
-      <ListItem 
-        key={1}
-        avatar={avatar}
-        titulo="Igor Felipe Sales" 
-        description="Curso: Sistemas de Informação"
-        editLink="/document/form"
-        deleteLink="/document"
-      />
+      
+      { users.data.map( (user : User) => {
+        console.log(user)
+        
+        return(
+          <ListItem 
+            key={user.id}
+            avatar={avatar}
+            user={user} 
+            editLink="/users/form"
+            deleteLink="/users"
+          />
+        )}
+      )}
+
+      
 
     </PageBody>
   )
