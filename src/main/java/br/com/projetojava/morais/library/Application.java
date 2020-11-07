f0b2b7cdd66398096f9129f810dd45b02adc397f
ProjetoAlanaJava <73372877+ProjetoAlanaJava@users.noreply.github.com>
2020-11-07 12:43:45 -0300

    Revert "Main api"

--- src/main/java/br/com/projetojava/morais/library/Application.java
+++ src/main/java/br/com/projetojava/morais/library/Application.java
@@ -13,4 +13,15 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
