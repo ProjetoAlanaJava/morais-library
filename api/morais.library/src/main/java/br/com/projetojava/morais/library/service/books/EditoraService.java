ad48a51e8c6e380d1ff4dbb7eb0d096422097667
Igor Sales <igorsales.fs@gmail.com>
2020-11-12 01:37:19 -0300

    adicionando e configurando services de Autor, Categoria, Editora e Tipo

--- api/morais.library/src/main/java/br/com/projetojava/morais/library/service/books/EditoraService.java
+++ api/morais.library/src/main/java/br/com/projetojava/morais/library/service/books/EditoraService.java
@@ -1,8 +1,8 @@
 package br.com.projetojava.morais.library.service.books;
 
-import br.com.projetojava.morais.library.model.books.Autor;
 import br.com.projetojava.morais.library.model.books.Editora;
 import br.com.projetojava.morais.library.repository.books.EditoraRepository;
+import br.com.projetojava.morais.library.service.books.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +15,42 @@ public class EditoraService {
     @Autowired
     private EditoraRepository editoraRepository;
 
+    public Editora save(Editora editora){
+        return editoraRepository.save(editora);
+    }
+
 
     public List<Editora> findAll(){
         return editoraRepository.findAll();
     }
 
-    public Optional<Editora> findById(Long id){
-        return editoraRepository.findById(id);
+    public Editora findById(Long id){
+        Optional<Editora> editora = editoraRepository.findById(id);
+        return editora.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public Editora create( Editora editora){
+        return  editoraRepository.save(editora);
+    }
+
+
+    public Editora update(Editora editora){
+        Editora newEditora = findById(editora.getId());
+
+        updateData(newEditora, editora);
+        return save(editora);
+    }
+
+    public void updateData(Editora newEditora, Editora editora){
+        newEditora.setNome(editora.getNome());
+        newEditora.setCnpj(editora.getCnpj());
+        newEditora.setEmail(editora.getEmail());
+        newEditora.setPais(editora.getPais());
+        newEditora.setTelefone(editora.getTelefone());
+    }
+
+    public void deleteById(Long id){
+        findById(id);
+        editoraRepository.deleteById(id);
     }
 }
