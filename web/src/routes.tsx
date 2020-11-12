bf3eded19fa9c253ecb4134684f7f639da8d2c75
Igor Sales <igorsales.fs@gmail.com>
2020-11-12 13:03:24 -0300

    configurando rotas privadas e de login

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Users from './Pages/UsersList';
 import Books from './Pages/BooksList';
+import Login from './Pages/Login';
+
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({...rest}) => {
+    return isAuthenticated() ? <Route {...rest}/> : <Redirect to="/login"/>
+}
 
 function Routes(){
   return (
     <Switch>
-      <Route path="/" exact component={ Home }/>
-      <Route path="/books" exact component={ Books}/>
-      <Route path="/users" exact component={ Users }/>
-      <Route path="*" component={() => <h1>Page not found</h1>} />
+      <Route path="/" exact component={Home}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path="/books" exact component={ Books}/>
+      <PrivateRoute  path="/users" exact component={ Users }/>
+      <Route  path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   )
 }
