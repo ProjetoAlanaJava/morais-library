d6fec0157d7787705a87c02988a65d2db70d8dc6
Igor Sales <igorsales.fs@gmail.com>
2020-11-12 13:02:41 -0300

    configurando lib toastfy para exibicao de alertas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 
 import Navbar from './components/Navbar';
@@ -16,6 +17,7 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Routes />
+          <ToastContainer autoClose={3000} position="top-center"/>
         </BrowserRouter>
     </Provider>
   );
