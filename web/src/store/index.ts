784926b383f3652c9589d6a3eeb4088121922f14
Igor Sales <igorsales.fs@gmail.com>
2020-12-01 15:00:32 -0300

    configurando books na store do redux

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -2,12 +2,14 @@ import { applyMiddleware, createStore, Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import { UsersState } from './modules/users/types';
+import { BooksState } from './modules/books/types';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
 export interface ApplicationState {
   users: UsersState,
+  books: BooksState,
 }
 
 const sagaMiddleware = createSagaMiddleware();
--- web/src/store/modules/rootSaga.ts
+++ web/src/store/modules/rootSaga.ts
@@ -1,11 +1,14 @@
 import { all, takeLatest} from 'redux-saga/effects'
 
 import { UsersTypes } from './users/types';
+import { BooksTypes } from './books/types';
 import { loadUsersSaga } from './users/saga';
+import { loadBooksSaga } from './books/saga';
 
 
 export default function* rootSaga(){
   return yield all([
     takeLatest(UsersTypes.LOAD_USERS_REQUEST, loadUsersSaga ),
+    takeLatest(BooksTypes.LOAD_BOOKS_REQUEST, loadBooksSaga ),
   ])
 } 
