9cc1832b4ff3864dfe8d05828409a3ee50c37831
Igor Sales <igorsales.fs@gmail.com>
2020-12-01 23:48:16 -0300

    adicionando tela EventsList

---
+++ web/src/Pages/EventsList/index.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import PageBody from '../../components/PageBody';
+import ListItem from '../../components/ListItem';
+
+import bookIcon from '../../assets/images/book.png';
+
+import { ApplicationState } from '../../store';
+import { loadEventsRequest } from '../../store/modules/events/actions';
+import { Event } from '../../store/modules/events/types';
+
+import './styles.css';
+
+function EventsList(){
+
+  const dispatch = useDispatch()
+
+  const { events } = useSelector( (state: ApplicationState) => state);
+
+  useEffect(() => {
+    dispatch(loadEventsRequest())
+  }, [dispatch]);
+  
+
+  return (
+    <PageBody 
+      title="Eventos - Lista"
+      link="/eventos/form"
+      isForm={false}
+    >
+            
+      { events.data.map( (event: Event) => {
+        console.log(event)
+        return       <div className="events-list">
+        <ListItem 
+            key={event.id}
+            avatar={bookIcon}
+            header={event.title}
+            description_one_title="Data"
+            description_one_value={event.date}
+            description_two_title="Horario de início"
+            description_two_value={event.horarioInicio}
+            additional_information_title="Status"
+            additional_information_value={event.status}
+            editLink="/events/form"
+            deleteLink="events"
+        />
+      </div>
+      })}
+
+
+    </PageBody>
+  )
+}
+
+export default EventsList;
