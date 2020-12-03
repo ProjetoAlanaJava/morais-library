a35d187617c93dde5f1c65cd68a14fca4491214e
Igor Sales <igorsales.fs@gmail.com>
2020-12-03 03:18:57 -0300

    ajustando store de eventos

--- web/src/store/modules/events/actions.ts
+++ web/src/store/modules/events/actions.ts
@@ -1,5 +1,9 @@
 import { action } from 'typesafe-actions';
 import { Event, EventsTypes } from './types';
 
+export const addEvent = (data: Event) => action(EventsTypes.ADD_EVENT,  { data });
 export const loadEventsRequest = () => action( EventsTypes.LOAD_EVENTS_REQUEST);
 export const loadEvents = (data: Event []) => action( EventsTypes.LOAD_EVENTS, { data } );
+export const showEvent = ( data: Event) => action(EventsTypes.SHOW_EVENT, { data });
+export const updateEvent = ( data: Event ) => action(EventsTypes.UPDATE_EVENT, { data } );
+export const deleteEvent = (id: number) => action(EventsTypes.DELETE_EVENT, { id });
--- web/src/store/modules/events/reducer.ts
+++ web/src/store/modules/events/reducer.ts
@@ -9,6 +9,7 @@ const INITIAL_STATE: EventsState = {
   data: [],
   isEdit: false,
   loading: false,
+  formData: undefined,
   error: false
 }
 
@@ -24,6 +25,33 @@ const reducer: Reducer<EventsState> = (state = INITIAL_STATE, action) : EventsSt
     case EventsTypes.LOAD_EVENTS:
       console.log('REDUCER - LOAD_EVENTS')
       return { ...state,  data: action.payload.data, loading: false, error: false,  isEdit: false};
+
+    
+
+    case EventsTypes.SHOW_EVENT:
+      var eventId = action.payload.data.id;
+      console.log('REDUCER - SHOW_EVENT')
+      const event = state.data.filter( (event) => {
+        if(eventId === event.id){
+          return event;
+        }
+        return null
+      })
+
+    return { ...state, isEdit: true,  data: event, formData: event[0]}
+
+    case EventsTypes.UPDATE_EVENT:
+      const eventUpdate = action.payload.data;
+      console.log('UPDATE_EVENT ',eventUpdate)
+      return { ...state, isEdit: false, formData: undefined}
+
+    case EventsTypes.DELETE_EVENT:
+      var eventDeleteId = action.payload.id;
+      console.log('DELETE_EVENT')
+      console.log(eventDeleteId)
+      return {...state, data: state.data.filter( event => 
+        event.id !== eventDeleteId
+      )};
 
     default:
       return state
