34bb9089f2edf37c2435ec307dab3fed0e61dd40
Igor Sales <igorsales.fs@gmail.com>
2020-12-04 02:30:23 -0300

    configurando root e index para redux de login e reservas

--- web/src/store/modules/rootReducer.ts
+++ web/src/store/modules/rootReducer.ts
@@ -4,10 +4,12 @@ import users from './users/reducer';
 import books from './books/reducer';
 import events from './events/reducer';
 import spaces from './spaces/reducer';
+import login from './login/reducer';
 
 export default combineReducers({
   users,
   books,
   events,
-  spaces
+  spaces,
+  login,
 });
--- web/src/store/modules/rootSaga.ts
+++ web/src/store/modules/rootSaga.ts
@@ -3,7 +3,7 @@ import { all, takeLatest} from 'redux-saga/effects'
 import { UsersTypes } from './users/types';
 import { loadUsersSaga } from './users/saga';
 import { BooksTypes } from './books/types';
-import { loadBooksSaga } from './books/saga';
+import {  loadBooksReservationsSaga, loadBooksSaga } from './books/saga';
 import { EventsTypes } from './events/types';
 import { loadEventsSaga } from './events/saga';
 import { SpacesTypes } from './spaces/types';
@@ -14,6 +14,7 @@ export default function* rootSaga(){
   return yield all([
     takeLatest(UsersTypes.LOAD_USERS_REQUEST, loadUsersSaga ),
     takeLatest(BooksTypes.LOAD_BOOKS_REQUEST, loadBooksSaga ),
+    takeLatest(BooksTypes.LOAD_BOOKS_RESERVATIONS_REQUEST, loadBooksReservationsSaga ),
     takeLatest(EventsTypes.LOAD_EVENTS_REQUEST, loadEventsSaga ),
     takeLatest(SpacesTypes.LOAD_SPACES_REQUEST, loadSpacesSaga ),
   ])
